a1e73ad60bd422c9e62043b1570ff24f92dae85a
Lucas Nascimento <lucnascimento98@gmail.com>
2022-07-04 08:26:27 -0300

    Adicionando restricoes

--- SimulatedAnneling/SimulatedAnneling/Program.cs
+++ SimulatedAnneling/SimulatedAnneling/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 List<string> Dias = new() { "Seg", "Ter", "Qua", "Qui", "Sex" };
 List<string> Enfermeiros = new() { "Ana", "Beto", "Carla", "David", "Emanuel", "Fabiana", "Gabriel", "Gabriel2", "Gabriel3", "Gabriel4", "Gabriel5" };
 List<int> Turnos = new() { 1, 2, 3 };
-List<int> CargaHoraria = new() { 40, 40, 40, 40, 40, 40, 40 };
+List<int> CargaHoraria = new() { 40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40 };
 List<int> qtdHoras = new() { 8, 8, 8 };
 
 int[,,] X = new int[Enfermeiros.Count, Dias.Count, Turnos.Count];
@@ -67,7 +67,7 @@ double func(int[,,] Custo, int[,,] X) //calculo da funcao objetivo
 }
 double accept(double z, double maximize, double T)
 {
-    return Math.Min(1, Math.Pow(Math.Exp(1), -(Math.Abs(maximize - z)) / T));
+    return Math.Min(1, Math.Pow(Math.Exp(1), -(maximize - z) / T));
     //return Math.Pow(Math.Exp(1), p);
 }
 double fRand(double fMin, double fMax)
@@ -75,40 +75,93 @@ double fRand(double fMin, double fMax)
     double f = (double)new Random().Next() / (double)int.MaxValue;
     return fMin + f * (fMax - fMin);
 }
-double Factivel(int[,,] X, double z)
+double Factivel(int[,,] X, double z, ref int violacoes)
 {
-    //int horasTrabalhadas = 0;
-
-    //foreach (var e in Enfermeiros)  
-    //{
-    //    foreach (var d in Dias)
-    //    {
-    //        foreach (var t in Turnos)
-    //        {
-    //            horasTrabalhadas += X[Enfermeiros.IndexOf(e), Dias.IndexOf(d), Turnos.IndexOf(t)]*qtdHoras[Turnos.IndexOf(t)];
-    //        }
-    //    }
-    //    if (horasTrabalhadas != CargaHoraria[Enfermeiros.IndexOf(e)]) return "CH " + e;
-    //    horasTrabalhadas = 0;
-    //}
-    int alocados = 0;
+    int sum = 0;
+    violacoes = 0;
+    #region Carga Horaria
+    foreach (var e in Enfermeiros)
+    {
+        foreach (var d in Dias)
+        {
+            foreach (var t in Turnos)
+            {
+                sum += X[Enfermeiros.IndexOf(e), Dias.IndexOf(d), Turnos.IndexOf(t)] * qtdHoras[Turnos.IndexOf(t)];
+            }
+        }
+        if (sum != CargaHoraria[Enfermeiros.IndexOf(e)])
+        {
+            z -= 10;
+            violacoes++;
+            //Console.WriteLine("CH");
+        }
+        sum = 0;
+    }
+    #endregion
+    #region Demanda minima e maxima
+    sum = 0;
     foreach (var d in Dias)
     {
         foreach (var t in Turnos)
         {
             foreach (var e in Enfermeiros)
             {
-                alocados += X[Enfermeiros.IndexOf(e), Dias.IndexOf(d), Turnos.IndexOf(t)];
+                sum += X[Enfermeiros.IndexOf(e), Dias.IndexOf(d), Turnos.IndexOf(t)];
             }
-            if (alocados < dMin[Dias.IndexOf(d), Turnos.IndexOf(t)] || alocados > dMax[Dias.IndexOf(d), Turnos.IndexOf(t)])
+            if (sum < dMin[Dias.IndexOf(d), Turnos.IndexOf(t)] || sum > dMax[Dias.IndexOf(d), Turnos.IndexOf(t)])
             {
                 z -= 10;
                 //z *= 0.1;
                 //return "Demanda";
+                violacoes++;
+                //Console.WriteLine("Demanda");
+
             }
-            alocados = 0;
+            sum = 0;
         }
     }
+    #endregion
+    #region Apenas 1 turno por dia
+    sum = 0;
+    for (int e = 0; e < Enfermeiros.Count; e++)
+    {
+        for (int d = 0; d < Dias.Count; d++)
+        {
+            for (int t = 0; t < Turnos.Count; t++)
+            {
+                sum += X[e,d,t];
+            }
+            if (sum > 1)
+            {
+                z -= 10;
+                violacoes++;
+                //Console.WriteLine("apenas 1 turno");
+
+            }
+            sum = 0;
+        }
+    }
+    #endregion
+    #region Proibiição de turnos subsequentes
+    sum = 0;
+    for (int e = 0; e < Enfermeiros.Count; e++)
+    {
+        for (int d = 0; d < Dias.Count-1; d++)
+        {
+            sum += X[e, d, 2]
+            + X[e, d + 1, 0] 
+            + X[e, d + 1, 1];
+            if (sum > 1)
+            {
+                z -= 10;
+                violacoes++;
+                //Console.WriteLine($"turnos subsequentes {e} {d}");
+
+            }
+            sum = 0;
+        }
+    }
+    #endregion
     return z;
 }
 
@@ -127,40 +180,42 @@ void MostraX(int[,,] X)
         Console.WriteLine();
     }
 }
-var random = new Random();
+
 for (double porcentagem = 0.5; porcentagem < 5; porcentagem += 0.5)
 {
-    //X = new int[,,]{
-    //    {{1,0,1}, {1,0,1}, {1,1,0}, {1,1,1}, {1,1,1}},
-    //    {{0,0,1}, {0,0,1}, {1,1,1}, {0,0,1}, {1,0,1}},
-    //    {{1,0,0}, {1,0,0}, {0,1,1}, {0,0,1}, {0,1,1}},
-    //    {{1,0,1}, {1,0,1}, {0,0,1}, {0,1,1}, {1,1,0}},
-    //    {{1,1,1}, {0,1,1}, {1,0,1}, {0,0,1}, {1,0,1}},
-    //    {{1,1,1}, {0,1,1}, {0,0,1}, {1,1,1}, {0,0,1}}, //fabiana
-    //    {{1,1,0}, {1,1,1}, {0,0,0}, {0,0,0}, {0,0,0}}, //gabriel
-    //    {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}},
-    //    {{0,0,0}, {0,0,0}, {0,0,0}, {1,0,0}, {0,0,0}},
-    //    {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}},
-    //    {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}},
-    //};
-    foreach (var enf in Enfermeiros)
-    {
-        foreach (var dia in Dias)
-        {
-            foreach (var turno in Turnos)
-            {
-                X[Enfermeiros.IndexOf(enf), Dias.IndexOf(dia), Turnos.IndexOf(turno)] = random.Next(2);
-            }
-        }
-    }
+    X = new int[,,]{
+        {{1,1,1}, {1,0,0}, {1,0,0}, {1,0,0}, {1,0,0}},
+        {{1,0,0}, {1,0,0}, {1,0,0}, {1,0,0}, {1,0,0}},
+        {{1,0,0}, {1,0,1}, {1,0,0}, {1,0,0}, {1,0,0}},
+        {{1,0,0}, {1,0,0}, {1,0,0}, {1,0,0}, {1,0,0}},
+        {{1,0,0}, {1,0,0}, {1,0,0}, {1,1,0}, {1,0,0}},
+        {{0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}}, //fabiana
+        {{0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}}, //gabriel
+        {{0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}, {0,1,0}},
+        {{0,1,1}, {0,0,0}, {0,0,1}, {0,0,1}, {0,0,1}},
+        {{0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}},
+        {{0,0,1}, {0,0,1}, {0,0,1}, {0,0,1}, {1,0,1}},
+    };
+    //foreach (var enf in Enfermeiros)
+    //{
+    //    foreach (var dia in Dias)
+    //    {
+    //        foreach (var turno in Turnos)
+    //        {
+    //            X[Enfermeiros.IndexOf(enf), Dias.IndexOf(dia), Turnos.IndexOf(turno)] = new Random().Next(2);
+    //        }
+    //    }
+    //}
 
     //MostraX(X);
-    //int[,,] XN = (int[,,])X.Clone();
     int[,,] XM = (int[,,])X.Clone();
     int[,,] XBOF = (int[,,])X.Clone();
+    
+    int violacoes = 0;
 
-    double z = Factivel(X, func(Custo, X));
-    Console.Write("z inicial = " + z);
+    double z = Factivel(X, func(Custo, X), ref violacoes);
+    Console.Write($"z inicial = {z} \t violacoes = {violacoes}\t------" );
+
     double maximize = z;
     double BOF = z;
 
@@ -187,7 +242,7 @@ for (double porcentagem = 0.5; porcentagem < 5; porcentagem += 0.5)
                 X[enf, dia, turno] = 1 - XM[enf, dia, turno];
             }
 
-            z = Factivel(X, func(Custo, X));
+            z = Factivel(X, func(Custo, X), ref violacoes);
 
             if (z > maximize)
             {
@@ -214,6 +269,7 @@ for (double porcentagem = 0.5; porcentagem < 5; porcentagem += 0.5)
                 {
                     aceito = false;
                     X = ((int[,,])XM.Clone());
+                    XM = (int[,,])XBOF.Clone();
                 }
             }
 
@@ -251,23 +307,28 @@ for (double porcentagem = 0.5; porcentagem < 5; porcentagem += 0.5)
             //}
 
 
-            Console.WriteLine(
-                " Z = " + Math.Round(z, 6) +
-                "\t\t\t max = " + Math.Round(maximize, 6) +
-                "\t\t\t T = " + Math.Round(T, 6) +
-                "\t\t\t BOF = " + Math.Round(BOF, 6) +
-                //"\t\t\t ac = " + Math.Round(ac, 6) +
-                "\t\t\t ac = " + aceito);
+            //Console.WriteLine(
+            //    " Z = " + Math.Round(z, 6) +
+            //    "\t\t\t max = " + Math.Round(maximize, 6) +
+            //    "\t\t\t T = " + Math.Round(T, 6) +
+            //    "\t\t\t BOF = " + Math.Round(BOF, 6) +
+            //    //"\t\t\t ac = " + Math.Round(ac, 6) +
+            //    "\t\t\t ac = " + aceito);
         }
         T *= a;
         //Task.Delay(100).Wait();
     }
+
+    Factivel(XBOF, func(Custo, XBOF), ref violacoes);
     Console.WriteLine(
         "\tmax: " + Math.Round(maximize, 3) +
-        "\t\tBOF = " + Math.Round(BOF, 3) + " " +
+        "\tBOF = " + Math.Round(BOF, 3) + " " +
         "\tporcentagem = " + porcentagem + " " +
-        "\talocacoes trocadas = " + alocacoesTrocadas);
-    //MostraX(XM);
+        "\talocacoes trocadas = " + alocacoesTrocadas +
+        "\tviolacoes = " + violacoes);
+    Console.WriteLine();
+    //MostraX(XBOF);
+
     //Console.ReadLine();
 }
 
